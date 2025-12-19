6be048a87d9b4ee745120c3e594e078fe4dfad1e
Mustakim Ali <mustakimalikiash@gmail.com>
2025-12-20 01:26:21 +0600

    Create Index.php

---
+++ Online Shopping System/Index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Online Shopping System</title>
+    </head>
+    <body>
+        <h1>Welcome to Our Online Shopping System</h1>
+        <p>Explore our products and enjoy shopping!</p>
+    </body>
+</html>
