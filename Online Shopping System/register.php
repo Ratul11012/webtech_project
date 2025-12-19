cad27e5cb8cbea3225b7bd8edf9d54c9cf2e10a6
Ratul0012 <fahimchowdhuryratul009@gmail.com>
2025-12-20 02:31:08 +0600

    Update register.php

--- Online Shopping System/register.php
+++ Online Shopping System/register.php
@@ -2,9 +2,28 @@
 <html>  
     <head> 
     <title>Document</title>
+    <style>
+    .registerdiv{
+        display: flex;
+        flex-wrap:wrap;
+        flex-direction: row;
+        justify-content: center;
+        
+    }
+    .registerdiv a{
+        text-decoration: none;
+        background-color: lightgreen;
+        padding: 10px;
+    }
+</style>
     </head>
     <body>
-<form action="register.php"  method="post">
+    
+<div class="registerdiv">
+    <a href="index.php">Shop</a>
+<div>  
+    
+    <form action="register.php"  method="post">
     <input type="text" name="name" placeholder="Enter your name here!" required>
      <input type="email" name="email" placeholder="Enter your email here!" required>
         <input type="password" name="password" placeholder="Enter your password here!" required>
@@ -12,6 +31,10 @@
          <textarea name ="address" placeholder="Enter your address here!" required></textarea>
          <input type="submit" name="submit" value="sign up"> 
 </form>
+</div>
+</div>
+
+
 
 </body>
 </html>
