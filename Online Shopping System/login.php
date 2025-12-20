186e980dfc089040f565d0d90a30f77c0ef08ff3
Ratul0012 <fahimchowdhuryratul009@gmail.com>
2025-12-20 17:58:21 +0600

    Update login.php

--- Online Shopping System/login.php
+++ Online Shopping System/login.php
@@ -1,8 +1,20 @@
+<?php
+
+
+?>
+
 <!DOCTYPE html>
 <html>  
     <head>
     <title>Document</title>
     </herad>
-    <body>  
+    <body> 
+        <form action="login.php" method="post">
+        <input type="email" name="email" placeholder="Enter your email here!" required>
+        <input type="password" name="password" placeholder="Enter your password here!" required>
+        <input type="submit" name="submit" value="login">
+        <p> Don't Register Yet!<a href="register.php"> Sign Up</a></p>
+    
+    </form> 
     </body>
     </html>
