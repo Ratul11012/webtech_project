2a8936a1a5d5d88d751f4fdfa30128a7bf185258
Mustakim Ali <mustakimalikiash@gmail.com>
2025-12-25 19:32:11 +0600

    Refactor admin dashboard and fix PHP syntax errors
    Corrected PHP opening tag and improved session handling logic. Fixed CSS typo for sidebar width, updated logout link to point to logout.php, and replaced placeholder text with a welcome message in the admin dashboard.

--- Online Shopping System/admin/dashboard.php
+++ Online Shopping System/admin/dashboard.php
@@ -1,84 +1,76 @@
-<?<php>
+<?php
+session_start();  
 
-session_start();
-if(isset($_SESSION['user_id'])){
-    
-    if($_SESSION['user_role'] =='admin'){
+if (isset($_SESSION['user_id'])) {
+
  
+    if ($_SESSION['user_role'] == 'admin') {
+   
+    } else {
+        echo "Go to user dashboard";
     }
-    else{
-        echo"go to user dashboard";
-    }
-    
-}
 
-else{
+} else {
+
     header("Location: ../index.php");
     exit();
 }
 ?>
 
-
 <!DOCTYPE html>
 <html>
     <head>
         <title>ASHTASY BD</title>
-
         <style> 
+            * {
+                margin: 0;
+                padding: 0;
+            }
 
-        *{
-            margin:0;
-            padding:0;
-        }
+            .dashboard_sidebar {
+                position: fixed;
+                top: 0;
+                background-color: darkcyan;
+                width: 200px;  
+                height: 100%;
+            }
 
-        .dashboard_sidebar{
-            position: fixed;
-            top:0;
-            background-color:darkcyan;
-            width: 200x;
-            height: 100%;
-        }
+            .dashboard_sidebar ul li {
+                list-style: none;
+                text-align: center;
+            }
 
-        .dashboard_sidebar ul li{
-            list-style: none;
-            text-align: center;
-            
-        }
+            .dashboard_sidebar ul li a {
+                display: block;
+                text-decoration: none;
+                color: white;
+                padding: 10px;
+            }
 
-        .dashboard_sidebar ul li a{
-            display: block;
-            text-decoration: none;
-            color:white;
-            padding: 10px;
-        }
+            .dashboard_sidebar ul li a:hover {
+                background-color: black;
+            }
 
-        .dashboard_sidebar ul li a:hover{
-            background-color: black;
-        }
-
-
-        .dashboard_main{
-            margin-left: 200px;
-            padding: 30px;
-        }
-
+            .dashboard_main {
+                margin-left: 200px;
+                padding: 30px;
+            }
         </style>
     </head>
 
     <body>
 
-       <div class="dashboard_sidebar">
-       <ul> 
-        <li><a href="">Add Product</a> </li>
-        <li><a href="">View Orders</a> </li>
-        <li> <a href="">Logout</a> </li>
-       </ul>
-       </div>
+        <div class="dashboard_sidebar">
+            <ul> 
+                <li><a href="">Add Product</a> </li>
+                <li><a href="">View Orders</a> </li>
+                <li><a href="logout.php">Logout</a> </li> 
+            </ul>
+        </div>
        
-       <div class="dashboard_main">
-       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores enim ab ut in esse qui iure ea, recusandae rerum commodi quo corrupti quos itaque beatae possimus! Deserunt voluptates modi sint. </p>
-       </div>
-
+        <div class="dashboard_main">
+            <p>Welcome to the Admin Dashboard!</p>  
+        </div>
 
     </body>
 
