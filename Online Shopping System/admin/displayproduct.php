18e649375b5d7379d4ef19462a0d4e4e5feaf5e8
Mustakim Ali <mustakimalikiash@gmail.com>
2025-12-27 13:43:58 +0600

    Update displayproduct.php

--- Online Shopping System/admin/displayproduct.php
+++ Online Shopping System/admin/displayproduct.php
@@ -40,9 +40,48 @@ else{
 
     <head>
         <title>ASHTASY BD</title>
+
+        <style>
+
+        </style>
     </head>
 
     <body>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Product Title</th>
+                <th>Product Description</th>
+                <th>Price</th>
+                <th>Stock</th>
+                <th>Image</th>
+                <th>Category Name</th>
+                <th>Action</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php 
+            while($row=mysqli_fetch_assoc($result)){
+
+            
+            ?>
+            <tr>
+                <td><?php echo  </td>
+                <td>pd </td>
+                <td>24 </td>
+                <td>12 </td>
+                <td> img </td>
+                <td> catgn </td>
+                <td> <a class="update" href="#"> Update</a></td>
+                <td> <a class="delete" href="#"> Delete</a></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+
+    </table>
 
     </body>
 
