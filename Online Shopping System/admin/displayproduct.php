8eaf76c43594fb7a6a6d2d02034cf1049ef03530
Mustakim Ali <mustakimalikiash@gmail.com>
2025-12-27 13:59:31 +0600

    Add sidebar and main content CSS styles
    Introduced new CSS rules for a fixed sidebar and main content area, including layout, colors, and hover effects for navigation links to improve the admin dashboard's appearance.

--- Online Shopping System/admin/displayproduct.php
+++ Online Shopping System/admin/displayproduct.php
@@ -43,6 +43,42 @@ else{
 
         <style>
 
+            <style> 
+        
+            * {
+                margin: 0;
+                padding: 0;
+            }
+
+            .dashboard_sidebar {
+                position: fixed;
+                top: 0;
+                background-color: darkcyan;
+                width: 200px;  
+                height: 100%;
+            }
+
+            .dashboard_sidebar ul li {
+                list-style: none;
+                text-align: center;
+            }
+
+            .dashboard_sidebar ul li a {
+                display: block;
+                text-decoration: none;
+                color: white;
+                padding: 10px;
+            }
+
+            .dashboard_sidebar ul li a:hover {
+                background-color: black;
+            }
+
+            .dashboard_main {
+                margin-left: 200px;
+                padding: 30px;
+            }
+
         </style>
     </head>
 
