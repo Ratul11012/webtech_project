4935c8ea6714b357acb292db5effee91ebba2153
Mustakim Ali <mustakimalikiash@gmail.com>
2026-01-05 12:10:37 +0600

    Update Index.php

--- Online Shopping System/Index.php
+++ Online Shopping System/Index.php
@@ -97,7 +97,7 @@
         .product:hover{
             box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
             transform: translateY(-5px);
-            transition: all 0.3s ease;
+        
         }
 
           .product a{
