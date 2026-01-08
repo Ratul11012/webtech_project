a497971ae48350d4eb6669a7c6f7a1208d42ee21
Ratul0012 <fahimchowdhuryratul009@gmail.com>
2026-01-08 14:33:37 +0600

    Update myorders.php

--- Online Shopping System/myorders.php
+++ Online Shopping System/myorders.php
@@ -115,10 +115,9 @@ else{
 
     <div class="dashboard_sidebar">
             <ul> 
-                
+                 <li><a href="index.php">Shop</a> </li>
                 <li><a href="myorders.php">My Order</a> </li>
-                 
-               
+                               
                 <li><a href="logout.php">Logout</a> </li> 
             </ul>
         </div>
