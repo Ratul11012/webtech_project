874c958c47891594adfc64b2560991edb6e50972
Mustakim Ali <mustakimalikiash@gmail.com>
2026-01-15 01:17:11 +0600

    Update updateproduct.php

--- Online Shopping System/admin/updateproduct.php
+++ Online Shopping System/admin/updateproduct.php
@@ -34,16 +34,43 @@ if (isset($_SESSION['user_id'])) {
             }
 
             $image=$_FILES['image']['name'];
-            if($image=){
-    
+            if($image){
             $temp_location=$_FILES['image']['tmp_name'];
             $upload_location= "../image/";
-            
-           
+
+            $sql4="update products set name='$name', description='$description', price='$price', stock='$stock', image='$image' where id='$product_id' ";
+            $result4=mysqli_query($conn, $sql4);
+            if($result4){
+                
+                echo "Product added successfully!";
+                move_uploaded_file($temp_location, $upload_location.$image);
+                header("Location: displayproduct.php");
+            }
+            else{
+                echo"ERROR! : {$conn->error}";
+            }
             
             }
 
              $category_name=$_POST['category_name'];
+             if($image){
+            $temp_location=$_FILES['image']['tmp_name'];
+            $upload_location= "../image/";
+
+            $sql4="update products set name='$name', description='$description', price='$price', stock='$stock', image='$image' where id='$product_id' ";
+            $result4=mysqli_query($conn, $sql4);
+            if($result4){
+                
+                echo "Product added successfully!";
+                move_uploaded_file($temp_location, $upload_location.$image);
+                header("Location: displayproduct.php");
+            }
+            else{
+                echo"ERROR! : {$conn->error}";
+            }
+            
+            }
+
 
             if(!$result){
                 echo"ERROR! : {$conn->error}";
