573a1335aaf6dde203578cdd95f602c20be2fdb4
Mustakim Ali <mustakimalikiash@gmail.com>
2026-01-19 00:22:49 +0600

    Refactor user auth to MVC structure and add styling
    Moved login, register, and logout PHP scripts into an MVC directory structure. Added separate HTML files for login and registration forms, along with dedicated CSS files for login, register, and dark mode. Introduced a dark mode toggle JavaScript. Updated image assets and relocated the database connection file. This improves maintainability and separates concerns between logic, presentation, and styling.

--- Online Shopping System/user/MVC/php/register.php
+++ Online Shopping System/user/MVC/php/register.php
@@ -1,5 +1,5 @@
 <?php
-include "db.php" ;
+include '../db/db.php';
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -32,82 +32,9 @@ if(isset($_POST['submit'])){
 <html>  
     <head>
     <title>Sign Up - ASHTASY BD</title>
-    <style>
-
-    body{
-        background-color: #0d1553a7;
-        font-family: Arial, sans-serif;
-        color: #333;
-
-    }
-
-
-
-    .registerdiv{
-        margin-top: 200px;
-        display: flex;
-        flex-wrap:wrap;
-        flex-direction: row;
-        justify-content: center;
-        
-    }
-    .brandName{
-       font-size: 24px; 
-       color: #ffd752ff;
-         text-decoration: none;
-            font-family:'lucida handwriting', cursive;
-            margin-left: 20px;
-            position: fixed;
-            top: 10px;
-            left: 20px;
-            
-        
-
-       
-         
-    }
-
-.registerdiv input{
-    display: block;
-    padding: 15px;
-    margin: 8px;
-
-
-}
-.registerdiv textarea{
-    display: block;
-    padding: 15px;
-    margin: 8px;
-    width: 162px;
-
-
-}
-.button{
-    width: 200px;
-    background-color: lightcoral;
-    border: none;
-
-}
-button:hover{
-    background-color: darkorange;
-
-   
-    
-}
-.registerdiv a{
-    color: black;
-    margin-left: 5px;
-    margin-top: 10px;
-
-   }
-</style>
+    <link rel="stylesheet" href="../css/register.css">
     </head>
     <body>
-
-
-
-
-
 
 <a href="index.php"  class="brandName" > ASHTASY </a>    
 
