b028b414f69837b9b8afc07eb3db35f584baa2e6
Ratul0012 <fahimchowdhuryratul009@gmail.com>
2026-01-19 19:20:50 +0600

    Update cart.php

--- Online Shopping System/user/MVC/php/cart.php
+++ Online Shopping System/user/MVC/php/cart.php
@@ -8,3 +8,27 @@ if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
+// Add product to the cart when "Buy Now" is clicked
+if (isset($_GET['product_id'])) {
+    $product_id = $_GET['product_id'];
+    $quantity = isset($_GET['quantity']) ? $_GET['quantity'] : 1;
+
+    // Fetch product details from the database
+    $sql = "SELECT * FROM products WHERE id = '$product_id'";
+    $result = mysqli_query($conn, $sql);
+    $product = mysqli_fetch_assoc($result);
+
+    if ($product) {
+        // Add to cart or update the quantity
+        if (isset($_SESSION['cart'][$product_id])) {
+            $_SESSION['cart'][$product_id]['quantity'] += $quantity;
+        } else {
+            $_SESSION['cart'][$product_id] = [
+                'name' => $product['name'],
+                'price' => $product['price'],
+                'quantity' => $quantity
+            ];
+        }
+    }
+}
+
